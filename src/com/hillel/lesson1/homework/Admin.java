72fd639dbe9f18a6f43137d143e5dae9eef7dd69
vladimirvasylenko <vasylenkovladimir10@gmail.com>
2021-06-14 21:29:09 +0300

    homework1 is done

--- src/com/hillel/lesson1/homework/Admin.java
+++ src/com/hillel/lesson1/homework/Admin.java
@@ -1,6 +1,6 @@
 package com.hillel.lesson1.homework;
 
-public class Admin extends Master{
+public class Admin extends Master {
 
     public Admin(String name, String surname, int age, String mail, String password, String role) {
         super(name, surname, age, mail, password, "ADMIN");
--- src/com/hillel/lesson1/homework/Main.java
+++ src/com/hillel/lesson1/homework/Main.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        User user = new User("Vladimir", "Vasylenko", 26,"vasylenko@mail.ua","321","u");
-        Admin admin = new Admin("Andrey", "Yashin", 20,"yashin@mail.ua","123","a");
+        User user = new User("Vladimir", "Vasylenko", 26, "vasylenko@mail.ua", "321", "u");
+        Admin admin = new Admin("Andrey", "Yashin", 20, "yashin@mail.ua", "123", "a");
 
         UserService userService = new UserService();
         AdminService adminService = new AdminService();
