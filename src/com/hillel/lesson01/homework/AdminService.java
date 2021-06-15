0b55614a0fdda8564cbab1854fc77400777ffe80
awesome <vasylenkovladimir10@gmail.com>
2021-06-15 12:51:42 +0300

    homework1 is done

--- src/com/hillel/lesson01/homework/AdminService.java
+++ src/com/hillel/lesson01/homework/AdminService.java
@@ -6,11 +6,6 @@ import java.io.*;
 public class AdminService extends ServiceAbstractClass {
 
     @Override
-    public boolean checkUser(Master user) throws IOException {
-        return super.checkUser(user);
-    }
-
-    @Override
     public void writeUser(Master user) throws IOException {
 
         FileWriter fileWriter = new FileWriter(file, true);
--- src/com/hillel/lesson01/homework/UserService.java
+++ src/com/hillel/lesson01/homework/UserService.java
@@ -5,11 +5,6 @@ import java.io.IOException;
 public class UserService extends ServiceAbstractClass {
 
     @Override
-    public boolean checkUser(Master user) throws IOException {
-        return super.checkUser(user);
-    }
-
-    @Override
     public void writeUser(Master user) {
         System.out.println("can't write with UserService");
     }
