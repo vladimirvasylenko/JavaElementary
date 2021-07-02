92206361aaf87ff7f6c13a86ce41f71028282462
vladimirvasylenko <vasylenkovladimir10@gmail.com>
2021-07-02 21:38:00 +0300

    homework6 50%

--- src/com/hillel/lesson6/homework/Main.java
+++ src/com/hillel/lesson6/homework/Main.java
@@ -1,10 +1,11 @@
 package com.hillel.lesson6.homework;
 
+import com.hillel.lesson6.homework.exceptions.StringFormatException;
 import com.hillel.lesson6.homework.utils.StringHelper;
 
 public class Main {
-    public static void main(String[] args) {
-        char[] charArray = {'a'};
+    public static void main(String[] args) throws StringFormatException {
+        char[] charArray = {'a','m','n'};
         String fuck = "fuck";
         StringHelper stringHelper = new StringHelper();
         stringHelper.arrayToStringWithCheck(fuck, charArray);
--- src/com/hillel/lesson6/homework/utils/StringHelper.java
+++ src/com/hillel/lesson6/homework/utils/StringHelper.java
@@ -1,22 +1,35 @@
 package com.hillel.lesson6.homework.utils;
 
-public class StringHelper {
-    public String arrayToStringWithCheck(String string, char[] charArray){
+import com.hillel.lesson6.homework.exceptions.StringFormatException;
 
-        if (charArray == null){
+public class StringHelper {
+    public String arrayToStringWithCheck(String string, char[] charArray) throws StringFormatException {
+
+        if (charArray == null) {
             throw new NullPointerException();
         }
-//        else{
-            String charArrayToString = String.valueOf(charArray);
-            System.out.println(charArrayToString);
-//        }
+        String charArrayToString = String.valueOf(charArray);
 
-        if (ch)
+        if (string.isBlank() || charArrayToString.isBlank()) {
+            throw new StringFormatException();
+        }
+
+        String stringToUpperCase = string.toUpperCase();
+        String charStringToLowerCase = charArrayToString.toLowerCase();
+
+        String concatString = stringToUpperCase.concat(" " + charStringToLowerCase);
+        String cutCarFromString;
+
+        if (concatString.length() % 2 != 0) {
+            cutCarFromString = concatString.substring(0, concatString.length() / 2) + concatString.substring(concatString.length()/2+1);
+            System.out.println(cutCarFromString);
+
+        } else {
+            cutCarFromString = concatString.substring(0, concatString.length() / 2 ) + concatString.substring(concatString.length()/2+2);
+            System.out.println(cutCarFromString);
+        }
 
 
-
-
-
-        return string;
+        return cutCarFromString;
     }
 }
