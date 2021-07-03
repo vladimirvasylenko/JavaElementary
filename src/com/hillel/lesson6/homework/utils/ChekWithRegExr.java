e3db1f849e80f7834afa9982943c621bf725168c
vladimirvasylenko <vasylenkovladimir10@gmail.com>
2021-07-03 15:54:09 +0300

    homework6 100%

--- src/com/hillel/lesson6/homework/utils/ChekWithRegExr.java
+++ src/com/hillel/lesson6/homework/utils/ChekWithRegExr.java
@@ -7,11 +7,11 @@ import java.util.regex.Pattern;
 public class ChekWithRegExr {
 
     public void chekPhoneNumber(String string) {
-        Pattern pattern = Pattern.compile("^\\+\\d{2}\\(\\d{3}\\)\\d{3}-\\d{2}-\\d{2}$");
-        Matcher matcher = pattern.matcher(string);
+        Pattern pattern = Pattern.compile("^\\+?38\\(?0\\d{2}\\)\\d{3}-\\d{2}-\\d{2}$");
+        Matcher matches = pattern.matcher(string);
 
-        if (matcher.find()) {
-            System.out.println(string.substring(matcher.start(), matcher.end()));
+        if (matches.find()) {
+            System.out.println(string.substring(matches.start(), matches.end()));
         } else {
             System.out.println("Incorrect phone number");
         }
@@ -19,10 +19,10 @@ public class ChekWithRegExr {
 
     public void chekEmail(String string) {
         Pattern pattern = Pattern.compile("^[A-Za-z0-9][A-Za-z0-9\\.\\-_]*[A-Za-z0-9]*@([A-Za-z0-9]+([A-Za-z0-9-]*[A-Za-z0-9]+)*\\.)+[A-Za-z]*$");
-        Matcher matcher = pattern.matcher(string);
+        Matcher matches = pattern.matcher(string);
 
-        if (matcher.find()) {
-            System.out.println(string.substring(matcher.start(), matcher.end()));
+        if (matches.find()) {
+            System.out.println(string.substring(matches.start(), matches.end()));
         } else {
             System.out.println("Incorrect email");
         }
@@ -30,10 +30,10 @@ public class ChekWithRegExr {
 
     public void chekDateOfBirth(String string) {
         Pattern pattern = Pattern.compile("^(?:0[1-9]|[12]\\d|3[01])([\\/.-])(?:0[1-9]|1[012])\\1(?:19|20)\\d\\d$");
-        Matcher matcher = pattern.matcher(string);
+        Matcher matches = pattern.matcher(string);
 
-        if (matcher.find()) {
-            System.out.println(string.substring(matcher.start(), matcher.end()));
+        if (matches.find()) {
+            System.out.println(string.substring(matches.start(), matches.end()));
         } else {
             System.out.println("Incorrect Date of Birth");
         }
